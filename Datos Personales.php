6b3b47a541acea941a14e2c5d5126a385184bc5e
Nadia <Nadia@nadia-a134df92e>
2015-12-02 16:17:22 -0300

    nadia

---
+++ Datos Personales.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "D.N.I: 38.472.088";
+        echo "L.U: 2879";
+        echo "MAIL: Nadialejandra_07@yahoo.com.ar";
+        echo "CELULAR: (3888) 15430565";
+        echo "NOMBRE Y APELLIDO: Nadia Alejandra Mamani";
+        ?>
+    </body>
+    
+</html>
